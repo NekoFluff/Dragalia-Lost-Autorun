a0683e382a12cb123449802e9f46c0ffe2beb101
CrazyFluff <alexnou@gmail.com>
2020-01-29 01:06:56 -0700

    Just some minor improvements to ai.py

--- python/ai.py
+++ python/ai.py
@@ -43,11 +43,13 @@ def auto_pic():
 
         index += 1
 
+
+# IDEA: Store check_if_exist results and reset after every screenshot
 def auto_run_dungeon(times):
+    real_time_image_path = './screenshots/_real-time.png'
     index = 0
     while(times > 0):
-        time.sleep(2)
-        real_time_image_path = './screenshots/_real-time.png'
+        time.sleep(2.5)
         screenshot(real_time_image_path)
         # press_next_if_ready()
         press_skip_if_ready()
@@ -60,9 +62,10 @@ def auto_run_dungeon(times):
         press_dragon_if_ready()
         press_auto_enable_if_disabled()
         # press_more_if_ready()
-        press_stamina_if_ready()
-        if press_repeat_if_ready():
+        if not check_if_exists('stamina', '_real-time') and not check_if_exists('confirm', '_real-time') and100press_repeat_if_ready():
             times -= 1
+        press_stamina_if_ready()
+
         # press_ticket_if_ready()
         press_confirm_if_ready()
 
@@ -71,6 +74,7 @@ def tap_at_point(x, y):
     subprocess.call(['adb', 'shell', 'input',
                          'tap', x, y])
     print('Tapped at', (x,y))
+    time.sleep(1.25)
 
 def press_next_if_ready():
     if check_if_exists('next', '_real-time'):
